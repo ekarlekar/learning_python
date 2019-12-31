a4bb10ce04ffba66de7170c2dddde2568aa49f2d
Esha Karlekar <ekarlekar@Eshas-MBP-3.fios-router.home>
2019-12-30 21:26:29 -0500

    my first attempt

--- project1.py
+++ project1.py
@@ -1,1 +1,12 @@
+import sys
+import time
+import cmd
 
+with open('words.txt') as f:
+	all_words = f.read().split('\n')
+
+s = String(input("please type a word:"))
+
+for x in range(len(all_words)):
+	if (s in x)
+		print(x)
