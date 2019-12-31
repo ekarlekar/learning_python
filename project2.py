718dfc59f528dad9765671656559870b54190f60
Esha Karlekar <ekarlekar@Eshas-MBP-3.fios-router.home>
2019-12-30 22:44:10 -0500

    doop de doop

--- project2.py
+++ project2.py
@@ -1,23 +1,43 @@
 import cmd
 import random
 
-with open('words.txt') as f:
-		all_words = f.read().split('/n')
+with open('words.txt', 'r') as f:
+		all_words = f.readLines()
+
+print(all_words)
+
+all_words = [x.strip('\n') for x in all_words]
+
 s = '' 
 for x in range(5):
-	print(x)
-	s+= random.choice(all_words)
+	print(random.choice(all_words))
 
-print(s)
 
-'''revstring = ''
 
-for i in s:
-	revstring = i + revstring
-print(revstring)
 
-finalS = ''
 
-for x in range(len(s)*2):
-	finalS += s[x] + revstring[x]
-print(finalS)'''
+
+
+
+
+
+
+
+
+
+
+
+
+#print(s)
+
+# revstring = ''
+
+# for i in s:
+# 	revstring = i + revstring
+# print(revstring)
+
+# finalS = ''
+
+# for x in range(len(s)*2):
+# 	finalS += s[x] + revstring[x]
+# print(finalS)
