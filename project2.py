be2d2ad78137a40cdf13ff22eba251111d8ef401
Esha Karlekar <ekarlekar@Eshas-MBP-3.fios-router.home>
2019-12-30 22:05:01 -0500

    first attempt

--- project2.py
+++ project2.py
@@ -1,1 +1,16 @@
+import cmd
 
+with open('words.txt') as f:
+		all_words = f.read().split('/n')
+s = '' 
+for x in range(5):
+	s+ = random.choice(all_words)
+print(s)
+revstring = ''
+for i in s:
+	revstring = i + revstring
+print(revstring)
+finalS = ''
+for x in range(len(s)*2))
+	finalS += s[x] + revstring[x]
+print(finalS)
