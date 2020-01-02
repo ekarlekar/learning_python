9d1b44b0d7e783891c3a47ea75bdf286bb1f267b
Esha Karlekar <ekarlekar@Eshas-MBP-3.fios-router.home>
2020-01-01 19:06:17 -0500

    made it into functions

--- project1.py
+++ project1.py
@@ -1,10 +1,11 @@
 import cmd
 
-with open('words.txt') as f:
+def main():
+    with open('words.txt') as f:
 	all_words = f.read().split('\n')
+	find_word(input("please type a word:"), all_words)
 
-s = input("please type a word:")
-
-for x in range(len(all_words)):
-	if (s in all_words[x]):
-		print(all_words[x])
+def find_word(find, findin):
+for x in range(len(findin)):
+	if (find in findin[x]):
+		print(findin[x])
